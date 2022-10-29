b15b28433be078f58bb45a310db5dd3218a11b12
xideaaa <ziebajakub_5@wp.pl>
2022-10-29 21:43:37 +0200

    boot v1.0.2

---
+++ src/python/otlichnyj_gorshok/Cons.py
@@ -0,0 +1,2 @@
+CONFIG_FILE = 'X:/$PyProjects/Otlichnyj-Gorshok/config/config.yml'
+QUEST_T1_FILE = 'X:/$PyProjects/Otlichnyj-Gorshok/quests/TutorialQuest1.yml'
---
+++ src/python/otlichnyj_gorshok/QuestYamlReader.py
@@ -0,0 +1,12 @@
+import yaml
+import Cons
+
+stream = open(Cons.QUEST_T1_FILE, 'r')
+dictionary = yaml.load(stream, Loader=yaml.FullLoader)
+for key, value in dictionary.items():
+    if key == 'StoryLine':
+        for k, v in dict(value).items():
+            print(f"v type {type(v)}") #sprawdzić czy jest listą jak jest to wybór
+            print(f"{k}: {v}")
+
+
