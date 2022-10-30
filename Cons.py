4ed5968eb92479e6777a88987fc62828b047acc2
xideaaa <ziebajakub_5@wp.pl>
2022-10-30 20:13:44 +0100

    edited Cons.py (added getGameDirectory(), getOperatingSystem() functions) moved quest.yml changed QuestYamlReader.py

---
+++ Cons.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+GAME_NAME = 'Otlichnyj-Gorshok'
+SYSTEM_NAME_LINUX = 'Linux'
+SYSTEM_NAME_MACOS = 'OS X'
+SYSTEM_NAME_WINDOWS = 'Windows'
+
+
+def getOperatingSystem():
+    platforms = {
+        'linux1': 'Linux',
+        'linux2': 'Linux',
+        'darwin': 'OS X',
+        'win32': 'Windows'
+    }
+    if sys.platform not in platforms:
+        return sys.platform
+    return platforms[sys.platform]
+
+
+def getGameDirectory():
+    path = os.getcwd().split('/')
+    home_path = ''
+    if getOperatingSystem() == SYSTEM_NAME_WINDOWS:
+        for dir in path:
+            home_path += f'/{dir}'
+            if dir == GAME_NAME:
+                break
+    else:
+        for dir in path[1:]:
+            home_path += f'/{dir}'
+            if dir == GAME_NAME:
+                break
+    return home_path
+
+
+QUEST_FILE = f'{getGameDirectory()}/config/quests.yml'
+CONFIG_FILE = f'{getGameDirectory()}/config/config.yml'
--- src/python/otlichnyj_gorshok/Cons.py
+++
@@ -1,2 +0,0 @@
-CONFIG_FILE = 'X:/$PyProjects/Otlichnyj-Gorshok/config/config.yml'
-QUEST_T1_FILE = 'X:/$PyProjects/Otlichnyj-Gorshok/quests/TutorialQuest1.yml'
