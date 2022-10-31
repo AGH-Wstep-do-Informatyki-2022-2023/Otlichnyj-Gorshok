cb30ec07746d426e5e4c395d5e0eaa80831097ae
xideaaa <ziebajakub_5@wp.pl>
2022-10-31 23:38:37 +0100

    updated quests files (quests.yml, QuestYamlReader.py) and added user.yml

--- Cons.py
+++ Cons.py
@@ -4,4 +4,4 @@ import yaml
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 QUEST_FILE = yaml.load(open(f'{ROOT_DIR}/config/quests.yml', 'r'), Loader=yaml.FullLoader)
 CONFIG_FILE = yaml.load(open(f'{ROOT_DIR}/config/config.yml', 'r'), Loader=yaml.FullLoader)
-
+USER_FILE = yaml.load(open(f'{ROOT_DIR}/config/user.yml', 'r'), Loader=yaml.FullLoader)
--- src/python/otlichnyj_gorshok/QuestYamlReader.py
+++ src/python/otlichnyj_gorshok/QuestYamlReader.py
@@ -1,7 +1,27 @@
 import Cons
 
-for key, value in Cons.QUEST_FILE.items():
-    if key == 'StoryLine':
-        for k, v in dict(value).items():
-            print(f"v type {type(v)}") #sprawdzić czy jest listą jak jest to wybór
-            print(f"{k}: {v}")
+
+def editSenderName(sender):
+    outputsender = ''
+    for letter in sender:
+        if letter != '_':
+            outputsender += letter
+        else:
+            break
+    return outputsender
+
+
+for questID, valueQuestID in Cons.QUEST_FILE.items():
+    if questID == Cons.USER_FILE['quest']:
+        for keyQuest, valueQuest in dict(valueQuestID).items():
+            if keyQuest == 'StoryLine':
+                for sender, message in dict(valueQuest).items():
+                    sender = editSenderName(sender)
+                    if type(message) is list:
+                        for index in range(0, len(message)):
+                            print(f"{index+1}) {message[index]}")
+                        choice = int(input("Wprowadź opcję wyboru: "))
+                        print(f"{sender}: {message[choice]}")
+                    else:
+                        print(f"{sender}: {message}")
+
