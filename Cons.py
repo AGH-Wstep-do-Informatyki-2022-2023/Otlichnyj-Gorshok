13d9d2e2dd9e2504a43ccd281e47cec4ba3c3944
xideaaa <ziebajakub_5@wp.pl>
2022-11-01 19:40:31 +0100

    QuestYamlReader.py added new functions (time_interval_for_reading, sender_message_analysis) changed gorshok_images file names added new lines to config.yml

--- Cons.py
+++ Cons.py
@@ -1,7 +1,13 @@
 import os
 import yaml
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = str(os.path.dirname(os.path.abspath(__file__))).replace('\\', '/')
 QUEST_FILE = yaml.load(open(f'{ROOT_DIR}/config/quests.yml', 'r'), Loader=yaml.FullLoader)
 CONFIG_FILE = yaml.load(open(f'{ROOT_DIR}/config/config.yml', 'r'), Loader=yaml.FullLoader)
 USER_FILE = yaml.load(open(f'{ROOT_DIR}/config/user.yml', 'r'), Loader=yaml.FullLoader)
+
+GORSHOK_LIST_IMAGES_TEMP = (os.listdir(f'{ROOT_DIR}/images/._gorshok_images'))
+# dict binds emotes and paths to their jpges
+GORSHOK_BIND_IMAGES = dict([((GORSHOK_LIST_IMAGES_TEMP[index].split('.'))[0],
+                             f'{ROOT_DIR}/images/._gorshok_images/{GORSHOK_LIST_IMAGES_TEMP[index]}')
+                            for index in range(0, len(GORSHOK_LIST_IMAGES_TEMP))])
--- src/python/otlichnyj_gorshok/QuestYamlReader.py
+++ src/python/otlichnyj_gorshok/QuestYamlReader.py
@@ -1,7 +1,12 @@
+from time import sleep
+
 import Cons
+import re
 
 
-def editSenderName(sender):
+# this function removes the string '_index' from the sender's name in quests.yml
+# e.g. Gorshok_11 -> Gorshok
+def change_sender_name(sender):
     outputsender = ''
     for letter in sender:
         if letter != '_':
@@ -11,17 +16,60 @@ def editSenderName(sender):
     return outputsender
 
 
+def sender_message_analysis(message):
+    cleanMessage = ''
+    emoteMessage = ''
+    for word in str(message).split(' '):
+        emote = re.search('<(.*)>', word)
+        if emote is None:
+            cleanMessage += word + ' '
+        else:
+            emoteMessage += str(emote.group(1))
+    return cleanMessage, emoteMessage
+
+
+def time_interval_for_reading(message):
+    word_per_minute = 300
+    second_per_word = 60 / word_per_minute
+    words = 0
+    if type(message) is list:
+        for choice in message:
+            words += len(str(choice).split(' '))
+    else:
+        words = len(str(message).split(' '))
+    return sleep(second_per_word * words)
+
+
+def quest_end():
+    return True
+
+
+def get_rewards():
+    return False
+
+
 for questID, valueQuestID in Cons.QUEST_FILE.items():
     if questID == Cons.USER_FILE['quest']:
         for keyQuest, valueQuest in dict(valueQuestID).items():
             if keyQuest == 'StoryLine':
+                print(valueQuestID['ShortDescription'])
+                print(valueQuestID['IntroText'])
                 for sender, message in dict(valueQuest).items():
-                    sender = editSenderName(sender)
+                    sender = change_sender_name(sender)
+                    # only player has choice
                     if type(message) is list:
                         for index in range(0, len(message)):
-                            print(f"{index+1}) {message[index]}")
-                        choice = int(input("Wprowadź opcję wyboru: "))
+                            print(f"{index}) {message[index]}")
+                        time_interval_for_reading(message)
+                        choice = int(input("Choose your option: "))
                         print(f"{sender}: {message[choice]}")
                     else:
-                        print(f"{sender}: {message}")
-
+                        print(f"{sender}: {sender_message_analysis(message)[0]}")
+                        time_interval_for_reading(message)
+                        if sender == Cons.CONFIG_FILE['gorshok_ingame_name']:
+                            print(f'{sender} emote: {sender_message_analysis(message)[1]}')
+                            print(f'{sender} emote path: '
+                                  f'{Cons.GORSHOK_BIND_IMAGES[sender_message_analysis(message)[1]]}')
+                        else:
+                            pass
+                print(valueQuestID['CompletedText'])
