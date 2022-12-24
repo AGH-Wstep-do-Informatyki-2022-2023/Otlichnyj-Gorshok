618a642813f91eab4364535401c03401fd2b83c5
xideaaa <ziebajakub_5@wp.pl>
2022-12-24 14:14:05 +0100

    images, window-manager

--- src/python/otlichnyj_gorshok/WDI/game_window.py
+++ src/python/otlichnyj_gorshok/WDI/game_window.py
@@ -1,14 +1,49 @@
-import pygame
-from src.python.otlichnyj_gorshok.util import Cons
+import tkinter as tk
 
-background_colour = (255,255,255)
-(width, height) = (300, 200)
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption(Cons.CONFIG_FILE['game_name'])
-screen.fill(background_colour)
-pygame.display.flip()
-running = True
-while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
+
+class MainWindow(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.geometry("800x600")
+
+        # Tworzenie paska po lewej stronie
+        left_frame = tk.Frame(self, width=200, height=600, bg="green")
+        left_frame.pack(side="left", fill="both", expand=True)
+
+        # Tworzenie paska po prawej stronie
+        right_frame = tk.Frame(self, width=600, height=600, bg="white")
+        right_frame.pack(side="right", fill="both", expand=True)
+
+        # Tworzenie przycisków
+        character_button = tk.Button(left_frame, text="Postać", command=self.show_character_frame)
+        map_button = tk.Button(left_frame, text="Mapa", command=self.show_map_frame)
+        skills_button = tk.Button(left_frame, text="Umiejętności", command=self.show_skills_frame)
+        character_button.pack()
+        map_button.pack()
+        skills_button.pack()
+
+        # Tworzenie ramki dla każdego okna
+        self.character_frame = tk.Frame(right_frame, width=600, height=600, bg="yellow")
+        self.map_frame = tk.Frame(right_frame, width=600, height=600, bg="blue")
+        self.skills_frame = tk.Frame(right_frame, width=600, height=600, bg="purple")
+
+    def show_character_frame(self):
+        self.character_frame.pack(side="right", fill="both", expand=True)
+        self.map_frame.pack_forget()
+        self.skills_frame.pack_forget()
+
+    def show_map_frame(self):
+        self.character_frame.pack_forget()
+        self.map_frame.pack(side="right", fill="both", expand=True)
+        self.skills_frame.pack_forget()
+
+    def show_skills_frame(self):
+        self.character_frame.pack_forget()
+        self.map_frame.pack_forget()
+        self.skills_frame.pack(side="right", fill="both", expand=True)
+
+
+if __name__ == "__main__":
+    app = MainWindow()
+    app.mainloop()
