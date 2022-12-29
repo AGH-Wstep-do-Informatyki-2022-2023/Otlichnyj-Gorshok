7e7c712233292102c29cac413a3cfac05e236701
xideaaa <ziebajakub_5@wp.pl>
2022-12-29 15:30:05 +0100

    images, window-manager

--- src/python/otlichnyj_gorshok/Otlichnyj_Gorshok.py
+++ src/python/otlichnyj_gorshok/Otlichnyj_Gorshok.py
@@ -1,4 +1,4 @@
-from src.python.otlichnyj_gorshok.window_manager.MainGameWindow import MainGameWindow
+from src.python.otlichnyj_gorshok.window.MainGameWindow import MainGameWindow
 
 if __name__ == "__main__":
     app = MainGameWindow()
