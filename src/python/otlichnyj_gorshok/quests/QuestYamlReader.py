930adfc786057c9c991e6b0a2cf9b9be94cecba0
xideaaa <ziebajakub_5@wp.pl>
2023-01-05 14:59:22 +0100

    quest, window #rzetelne

--- src/python/otlichnyj_gorshok/quests/QuestYamlReader.py
+++ src/python/otlichnyj_gorshok/quests/QuestYamlReader.py
@@ -3,27 +3,26 @@ from src.python.otlichnyj_gorshok.util import Cons
 
 util = QuestUtil()
 
-for questID, valueQuestID in Cons.QUEST_FILE.items():
-    if questID == Cons.USER_FILE['quest']:
-        for keyQuest, valueQuest in dict(valueQuestID).items():
-            if keyQuest == 'StoryLine':
-                print(valueQuestID['ShortDescription'])
-                print(valueQuestID['IntroText'])
-                for sender, message in dict(valueQuest).items():
-                    sender = util.change_sender_name(sender)
-                    if type(message) is list:
-                        for index in range(0, len(message)):
-                            print(f"{index}) {message[index]}")
-                        util.time_interval_for_reading(message)
-                        choice = int(input("Choose your option: "))
-                        print(f"{sender}: {message[choice]}")
-                    else:
-                        print(f"{sender}: {util.sender_message_analysis(message)[0]}")
-                        util.time_interval_for_reading(message)
-                        if sender == Cons.CONFIG_FILE['gorshok_ingame_name']:
-                            print(f'{sender} emote: {util.sender_message_analysis(message)[1]}')
-                            print(f'{sender} emote path: '
-                                  f'{Cons.GORSHOK_BIND_IMAGES[util.sender_message_analysis(message)[1]]}')
+def get_quest():
+    questStoryLineList = []
+    for questID, valueQuestID in Cons.QUEST_FILE.items():
+        if questID == Cons.USER_FILE['quest']:
+            for keyQuest, valueQuest in dict(valueQuestID).items():
+                if keyQuest == 'StoryLine':
+                    questStoryLineList.append(valueQuestID['IntroText'])
+                    for sender, message in dict(valueQuest).items():
+                        sender = util.change_sender_name(sender)
+                        if type(message) is list:
+                            for index in range(0, len(message)):
+                                print(f"{index}) {message[index]}")
+                            choice = int(input("Choose your option: "))
+                            print(f"{sender}: {message[choice]}")
                         else:
-                            pass
-                print(valueQuestID['CompletedText'])
+                            print(f"{sender}: {util.sender_message_analysis(message)[0]}")
+                            if sender == Cons.CONFIG_FILE['gorshok_ingame_name']:
+                                print(f'{sender} emote: {util.sender_message_analysis(message)[1]}')
+                                print(f'{sender} emote path: '
+                                      f'{Cons.GORSHOK_BIND_IMAGES[util.sender_message_analysis(message)[1]]}')
+                            else:
+                                pass
+                    print(valueQuestID['CompletedText'])
--- src/python/otlichnyj_gorshok/window/StartFrame.py
+++ src/python/otlichnyj_gorshok/window/StartFrame.py
@@ -1,10 +1,52 @@
 import tkinter as tk
-
+from PIL import Image, ImageTk
+import time
 
 class StartFrame(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
+        self.current_char_index = 0
+        self.parent = parent
 
-        self.image = tk.PhotoImage(file="X:\$PyProjects\Otlichnyj-Gorshok\\test.png")
-        self.label = tk.Label(self, image=self.image)
-        self.label.pack()
+        # Otwieranie zdjęcia i tworzenie tła
+        image = Image.open("X:\\$PyProjects\\Otlichnyj-Gorshok\\test.png")
+        image = image.resize((1024, 1024), Image.ANTIALIAS)
+        self.background_image = ImageTk.PhotoImage(image)
+        background_label = tk.Label(self, image=self.background_image)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        # Tworzenie ramki do wyświetlania tekstu
+        self.text_frame = tk.Frame(self, bg="white", bd=5)
+        self.text_frame.place(relx=0.5, rely=0.7, relwidth=0.9, relheight=0.2, anchor="n")
+
+        # Tworzenie listy tekstów do wyświetlenia
+        self.texts = ["Tekst 1 cacsca cs sac csa csa sc a sca sca csacsascac csac", "Teksascascascascasct 2", "Tecascascascascascascasc ascascbsauycvuaisvbc uyvasuy cvswuyc vwe3aucvbuw yevuyvfuyv uyv uevwfu yfvuwvefuywveuf  vweuyfvuwekst 3", "Tekst 4", ""]
+        self.current_text_index = 0
+
+        # Tworzenie etykiety do wyświetlania tekstu
+        self.text_label = tk.Label(self.text_frame, font=("Arial", 16), anchor="nw", justify="left", bd=0, wraplength=900)
+        self.text_label.place(relwidth=1, relheight=1)
+
+        # Odpalenie animacji tekstu
+        self.animate_text()
+
+    def animate_text(self):
+        # Wyświetlenie kolejnej litery tekstu
+        current_text = self.texts[self.current_text_index]
+        if self.current_char_index < len(current_text):
+            self.text_label.config(text=current_text[:self.current_char_index + 1])
+            self.current_char_index += 1
+            self.after(75, self.animate_text)
+        else:
+            # Po wyświetleniu całego tekstu, odczekaj 1 sekundę i wyświetl kolejny tekst lub przejdź do mapy
+            self.current_char_index = 0
+            if self.current_text_index < len(self.texts) - 1:
+                self.current_text_index += 1
+                self.after(2000, self.animate_text)
+            else:
+                self.parent.show_map_frame()
+
+
+    def show_map_frame(self):
+        self.pack_forget()
+        self.parent.map_frame.pack(side="right", fill="both", expand=True)
